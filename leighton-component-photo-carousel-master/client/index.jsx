5042e4fa6a10dcfe64903ae92d74a1191522c78d
philipstout3 <36464744+philipstout3@users.noreply.github.com>
2019-02-27 17:14:05 -0800

    Delete index.jsx

--- leighton-component-photo-carousel-master/client/index.jsx
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import PhotoCarousel from './components/PhotoCarousel';
-// import './styles/style.css';
-
-window.PhotoCarousel = PhotoCarousel;
